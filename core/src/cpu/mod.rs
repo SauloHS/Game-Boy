eca14d1356735569d92889f59241317ffdcef069
SauloHS <doroteios@gmail.com>
2026-08-01 21:11:52 -0300

    A bunch more opcodes

--- core/src/cpu/mod.rs
+++ core/src/cpu/mod.rs
@@ -181,6 +181,146 @@ impl Cpu {
         let inc = val.wrapping_add(1);
         self.set_r16(r, inc);
     }
+
+    pub fn and_a_u8(&mut self, val: u8) {
+        let mut a = self.get_r8(Regs::A);
+        a &= val;
+
+        self.set_r8(Regs::A, a);
+        self.set_flag(Flags::Z, a == 0);
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::H, true);
+        self.set_flag(Flags::C, false);
+    }
+
+    pub fn add_a_u8(&mut self, val: u8, adc: bool) {
+        let carry = if adc && self.get_flag(Flags::C) { 1 } else { 0 };
+        let a = self.get_r8(Regs::A);
+        let result1 = a.overflowing_add(val);
+        let h_check1 = check_h_carry_u8(a, val);
+        let result2 = result1.0.overflowing_add(carry);
+        let h_check2 = check_h_carry_u8(result1.0, carry);
+        let set_h = h_check1 || h_check2;
+        let set_c = result1.1 || result2.1;
+
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::C, set_c);
+        self.set_flag(Flags::H, set_h);
+        self.set_flag(Flags::Z, result2.0 == 0);
+        self.set_r8(Regs::A, result2.0);
+    }
+
+    pub fn sub_a_u8(&mut self, val: u8, sbc: bool) {
+        let mut carry = 0;
+        if sbc && self.get_flag(Flags::C) {
+            carry = 1;
+        }
+        let a = self.get_r8(Regs::A);
+        let result1 = a.overflowing_sub(val);
+        let check_h1 = check_h_borrow_u8(a, val);
+        let result2 = result1.0.overflowing_sub(carry);
+        let check_h2 = check_h_borrow_u8(result1.0, carry);
+        let set_h = check_h1 || check_h2;
+
+        self.set_flag(Flags::N, true);
+        self.set_flag(Flags::Z, result2.0 == 0);
+        self.set_flag(Flags::H, set_h);
+        self.set_flag(Flags::C, result1.1 || result2.1);
+        self.set_r8(Regs::A, result2.0);
+    }
+
+    pub fn cp_a_u8(&mut self, val: u8) {
+        let a = self.get_r8(Regs::A);
+        let set_h = check_h_borrow_u8(a, val);
+
+        self.set_flag(Flags::Z, a == val);
+        self.set_flag(Flags::N, true);
+        self.set_flag(Flags::H, set_h);
+        self.set_flag(Flags::C, a < val);
+    }
+
+    pub fn add_r16(&mut self, dst_r: Regs16, src_r: Regs16) {
+        let dst = self.get_r16(dst_r);
+        let src = self.get_r16(src_r);
+        let res = dst.overflowing_add(src);
+        let set_h = check_h_carry_u16(dst, src);
+
+        self.set_r16(dst_r, res.0);
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::H, set_h);
+        self.set_flag(Flags::C, res.1);
+    }
+
+    pub fn xor_a_u8(&mut self, val: u8) {
+        let mut a = self.get_r8(Regs::A);
+        a ^= val;
+        self.set_r8(Regs::A, a);
+        self.set_flag(Flags::Z, a == 0);
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::H, false);
+        self.set_flag(Flags::C, false);
+    }
+
+    pub fn or_a_u8(&mut self, val: u8) {
+        let mut a = self.get_r8(Regs::A);
+        a |= val;
+        self.set_r8(Regs::A, a);
+        self.set_flag(Flags::Z, a == 0);
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::H, false);
+        self.set_flag(Flags::C, false);
+    }
+
+    pub fn pop(&mut self) -> u16 {
+        assert_ne!(self.sp, 0xFFFE, "Trying to pop when the stack is empty");
+        let low = self.read_ram(self.sp);
+        let high = self.read_ram(self.sp + 1);
+        let val = merge_bytes(high, low);
+        self.sp += 2;
+        val
+    }
+
+    pub fn push(&mut self, val: u16) {
+        self.sp -= 2;
+        self.write_ram(self.sp, val.low_byte());
+        self.write_ram(self.sp, val.high_byte());
+    }
+
+    pub fn get_pc(&self) -> u16 {
+        self.pc
+    }
+
+    pub fn set_pc(&mut self, val: u16) {
+        self.pc = val;
+    }
+
+    pub fn rotate_left(&mut self, reg: Regs, carry: bool) {
+        let val = self.get_r8(reg);
+        let msb = val.get_bit(7);
+        let mut new = val.rotate_left(1);
+        if carry {
+            new.set_bit(0, self.get_flag(Flags::C));
+        }
+        self.set_r8(reg, new);
+        self.set_flag(Flags::Z, new == 0);
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::H, false);
+        self.set_flag(Flags::C, msb);
+    }
+
+    pub fn rotate_right(&mut self, reg: Regs, carry: bool) {
+        let val = self.get_r8(reg);
+        let lsb = val.get_bit(0);
+        let mut new = val.rotate_right(1);
+        if carry {
+            new.set_bit(7, self.get_flag(Flags::C));
+        }
+        self.set_r8(reg, new);
+        self.set_flag(Flags::Z, new == 0);
+        self.set_flag(Flags::N, false);
+        self.set_flag(Flags::H, false);
+        self.set_flag(Flags::C, lsb);
+    }
 }
 
 impl Default for Cpu {
